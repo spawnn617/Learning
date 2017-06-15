9b027ccb790ced47bbb4cdca0ffe5fa0e3e10e4c
DS <darius.siwek@gmail.com>
2017-06-15 11:34:37 +0200

    TEST

---
+++ CompareLoopTimes3/src/CompareLoopTimes3b.java
@@ -0,0 +1,32 @@
+/*
+ * Created by DS 2 on 15.06.2017.
+ */
+
+import java.time.LocalDateTime;
+
+public class CompareLoopTimes3b
+{
+    public static void main (String[] args)
+    {
+        int startTime, endTime;
+        final int REPEAT = 100_000;
+        final int FACTOR = 1_000_000;
+        LocalDateTime now;
+        now = LocalDateTime.now();
+        startTime = now.getNano();
+        for(int x = 0; x <= REPEAT; ++x)
+            for(int y = 0; y <= REPEAT; ++y);
+        now = LocalDateTime.now();
+        endTime = now.getNano();
+        System.out.println("Time for loops starting from 0: " +
+                ((endTime - startTime) / FACTOR) + " milliseconds");
+        now = LocalDateTime.now();
+        startTime = now.getNano();
+        for(int x = REPEAT; x >= 0; --x)
+        for(int y = REPEAT; y >= 0; --y);
+        now = LocalDateTime.now();
+        endTime = now.getNano();
+        System.out.println("Time for loops ending with 0: " +
+                ((endTime - startTime) / FACTOR) + " milliseconds");
+    }
+}
